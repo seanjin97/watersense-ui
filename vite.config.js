386ed736851f1d154b6e8f80c32d4c71daa9e0b1
Sean Jin <seanjin97@gmail.com>
2022-02-17 18:41:00 +0800

    bugfix(pwa testing): test remove vite pwa

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +7,5 @@ export default defineConfig({
 		react({
 			jsxRuntime: 'classic',
 		}),
-		VitePWA({}),
 	],
 });

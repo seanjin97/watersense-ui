3b5b26814159c4df857888fa1dd6a31319a7f0cc
kkkoh28 <79506724+kkkoh28@users.noreply.github.com>
2022-03-27 23:28:25 +0800

    test4

--- src/components/Overview/monthreview.jsx
+++ src/components/Overview/monthreview.jsx
@@ -1,10 +1,26 @@
 import React,{useState,useEffect} from "react";
+import { Container } from "react-bootstrap";
+import '../Overview/style.css';
 
 const monthreview = () => {
   return (
     <div>
         <h1>Monthly Overview</h1>
-        <div className = "reviewContainer"><h2>Month of March</h2></div>
+        <br></br>
+        <h3>Month of March</h3>
+        <Container>
+        <Container className = 'monthContainer' maxWidth="sm">
+            <p>Total Water Consumption for Month</p>
+            <p>Usage /Goal Set</p>
+            <p>Exceeded/Not exceeded</p>
+        </Container>
+
+        <Container className = 'dayContainer' maxWidth="sm">
+            <p>Total Days Exceeded</p>
+            <p>Days Exceeded /Days in Month</p>
+        </Container>
+        </Container>
+   
 
     </div>
   )
